b52f1e7136cfed05367207c8dcdbc7a49ca18c1c
Joseunghyeon96 <wer9698@naver.com>
2020-12-21 03:23:08 +0900

    branch setting

--- Python/10/10_02.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-window = Tk()
-window.title("B677035 조승현")
-window.geometry("600x400")
-window.resizable(width= FALSE, height = FALSE)
-
-window.mainloop()
--- Python/11/11_04.py
+++
@@ -1,9 +0,0 @@
-inFp= None
-inList,inStr = [],""
-inFp = open("D:/school/temp/data1.txt",'r')
-
-inList = inFp.readlines()
-for inStr in inList :
-    print(inStr,end= "")
-
-inFp.close()
